dc108e7cb4a9e224943065376656ebd7d0dabd1e
Vaccuss <Deansteven308@gmail.com>
2015-07-29 21:19:32 +1000

    Chart.js showing min and max temps for the day

--- Naiad/static/js/charting.js
+++ Naiad/static/js/charting.js
@@ -6,46 +6,51 @@ var randomScalingFactor = function () {
     return Math.round(Math.random() * 100)
 };
 
-
 var withData = function (callback) {
     $.post("/weather_data", 'Townsville', callback, "json");
 };
 
-withData(function (data, textStatus) {
-    var values = JSON.parse(data.responseJSON);
-    return {
-        labels: ["Day 0", "Day 1", "Day 2", "Day 3", "Day 4", "Day 5", "Day 6", "Day 7"],
-        datasets: [
-            {
-                label: "Minimum Temperatures",
-                fillColor: "rgba(220,220,220,0.2)",
-                strokeColor: "rgba(220,220,220,1)",
-                pointColor: "rgba(220,220,220,1)",
-                pointStrokeColor: "#fff",
-                pointHighlightFill: "#fff",
-                pointHighlightStroke: "rgba(220,220,220,1)",
-                data: [values.min_0, values.min_1, values.min_2, values.min_3, values.min_4, values.min_5,
-                    values.min_6, values.min_7]
-            },
-            {
-                label: "Maximum Tempretures",
-                fillColor: "rgba(220,220,220,0.2)",
-                strokeColor: "rgba(220,220,220,1)",
-                pointColor: "rgba(220,220,220,1)",
-                pointStrokeColor: "#fff",
-                pointHighlightFill: "#fff",
-                pointHighlightStroke: "rgba(220,220,220,1)",
-                data: [values.max_0, values.max_1, values.max_2, values.max_3, values.max_4, values.max_5,
-                    values.max_6, values.max_7]
-            }
-        ]
-    }
-});
 
 window.onload = function () {
     var ctx = document.getElementById("chart").getContext("2d");
-    window.myLine = new Chart(ctx).Line(withData(), {
-        responsive: true
+
+    withData(function (data, textStatus) {
+        var chartdata = {
+            labels: ["Day 0", "Day 1", "Day 2", "Day 3", "Day 4", "Day 5", "Day 6"],
+            datasets: [
+                {
+                    label: "Minimum Temperatures",
+                    fillColor: "rgba(220,220,220,0.2)",
+                    strokeColor: "rgba(220,220,220,1)",
+                    pointColor: "rgba(220,220,220,1)",
+                    pointStrokeColor: "#fff",
+                    pointHighlightFill: "#fff",
+                    pointHighlightStroke: "rgba(220,220,220,1)",
+                    data: [data.min_1, data.min_1, data.min_2, data.min_4, data.min_5,
+                        data.min_6, data.min_7].map(function (data) {
+                            var value = parseInt(data, 10);
+                            return Number.isNaN(value) ? 0 : value;
+                        })
+                },
+                {
+                    label: "Maximum Tempretures",
+                    fillColor: "rgba(220,220,220,0.2)",
+                    strokeColor: "rgba(220,220,220,1)",
+                    pointColor: "rgba(220,220,220,1)",
+                    pointStrokeColor: "#fff",
+                    pointHighlightFill: "#fff",
+                    pointHighlightStroke: "rgba(220,220,220,1)",
+                    data: [data.max_0, data.max_1, data.max_2, data.max_4, data.max_5,
+                        data.max_6, data.max_7].map(function (data) {
+                            var value = parseInt(data, 10);
+                            return Number.isNaN(value) ? 0 : value;
+                        })
+                }
+            ]
+        };
+        window.myLine = new Chart(ctx).Line(chartdata, {
+            responsive: true
+        });
     });
 };
 
